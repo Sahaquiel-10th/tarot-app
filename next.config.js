caad5b72c3e2d1297ad2628de729c2a163f46dba
Sahaquiel-10th <460402930@qq.com>
2025-11-10 18:17:41 +0800

    不要 basePath，让应用跑在根路径

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
-console.log('>>> Loading next.config.js (basePath=/tarot)')
+console.log('>>> Loading next.config.js (NO basePath)')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
   turbopack: { root: '.' },
-  basePath: '/tarot',
+  // 不要 basePath，让应用跑在根路径 /
 }

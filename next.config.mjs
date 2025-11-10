1787ad33759613296033ddea6b596be707bda863
Sahaquiel-10th <460402930@qq.com>
2025-11-10 15:21:41 +0800

    准备子路径部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  turbopack: {
-    root: ".",
-  },
+  basePath: '/tarot',
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  turbopack: { root: '.' },
 }
-
 export default nextConfig

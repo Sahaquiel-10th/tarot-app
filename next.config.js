be999c4084471b7a6220e704b6564a813c104dda
Sahaquiel-10th <460402930@qq.com>
2025-11-12 10:12:10 +0800

    哈哈，又改回来了！

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
-console.log('>>> Loading next.config.js (NO basePath)')
-
-/** @type {import('next').NextConfig} */
 module.exports = {
-  typescript: { ignoreBuildErrors: true },
+  basePath: '/tarot',
+  trailingSlash: true,
   images: { unoptimized: true },
-  turbopack: { root: '.' },
-  // 不要 basePath，让应用跑在根路径 /
+  output: 'export',
 }

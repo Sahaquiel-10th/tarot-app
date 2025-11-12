c2ee57fd8ddeadf77fe8a143b5ad8d501a50a9f5
Sahaquiel-10th <460402930@qq.com>
2025-11-12 10:20:02 +0800

    哈哈，又又改回来了！

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 module.exports = {
+  // 让站点挂在 /tarot
   basePath: '/tarot',
+  // 推荐加上，静态导出会生成 /tarot/index.html
   trailingSlash: true,
-  images: { unoptimized: true },
+
+  // 静态导出
   output: 'export',
-}
+
+  // 导出时禁用 next/image 的优化（否则会报错）
+  images: { unoptimized: true },
+
+  // 在客户端可读的前缀，代码里统一用它来拼路径
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '/tarot',
+  },
+};
